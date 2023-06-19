7bd7ae7a7cefb6f09b7c9a909ba27835b8824fc0
vitalMelnikofff <melnickov.vitalick2013@yandex.ru>
2023-06-19 23:51:22 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,33 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
-
+    public UserDao DAO = new UserDaoJDBCImpl();
+    public void createUsersTable() {DAO.createUsersTable();
+    }
+    public void dropUsersTable() {DAO.dropUsersTable();
     }
 
-    public void dropUsersTable() {
-
+    public void saveUser(String name, String lastName, byte age) {DAO.saveUser( name,  lastName, age);
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
-    public void removeUserById(long id) {
-
+    public void removeUserById(long id) {DAO.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = DAO.getAllUsers();
+        for (User user : users ){
+            System.out.println(user);
+        }
+        return users;
     }
 
-    public void cleanUsersTable() {
-
+    public void cleanUsersTable() {DAO.cleanUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
-}
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/task1";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }}
